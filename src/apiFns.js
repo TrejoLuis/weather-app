874815ef1a5b9e6029ca137e63501b7db5c3a076
Luis Trejo <trejorias@gmail.com>
2023-04-11 13:48:55 -0600

    fix: delete unused endpoint

--- src/apiFns.js
+++ src/apiFns.js
@@ -2,13 +2,12 @@
 const API_KEY = '1723de81b21140ddbac225948230304'
 const BASE_URL = 'https://api.weatherapi.com/v1/'
 
-const CURRENT_API = `${BASE_URL}current.json?key=${API_KEY}&q=`
 const FORECAST_API = `${BASE_URL}forecast.json?key=${API_KEY}&days=3&q=`
 
-//Manage both current and forecast endpoints
-async function weatherFetch(location, isCurrent){
+async function weatherFetch(location){
   try {
-    const response = isCurrent ? await fetch(CURRENT_API+location) : await fetch(FORECAST_API+location)
+    if(!location) return
+    const response = await fetch(FORECAST_API+location)
     if(response.status == 200) {
       return await response.json()
     } else {
